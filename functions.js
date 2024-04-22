2a8b54d13af8c5718918d3b73b6c28c65dec9cb4
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-04-22 19:15:17 +0300

    fatArrow

--- functions.js
+++ functions.js
@@ -3,7 +3,6 @@
 
 function denemeFonk(firstName){
     firstName = "deneme";
-    console.log(`Merhaba ${firstName}`);
     return fonk(firstName);
 }
 
@@ -13,3 +12,20 @@ function fonk(firstName){
 }
 
 denemeFonk();
+
+function denemeFat(lastName){
+    console.log(`Merhaba ${lastName}`);
+}
+denemeFat("lastname");
+
+const FatArrow = (lastName) => { console.log(`Merhaba ${lastName}`); }
+FatArrow("LASTNAME FAT ARROW");
+
+const FatArrow2 = (lastName, email) => { console.log(`Merhaba ${lastName} ${email}`); }
+FatArrow2("LASTNAME FAT ARROW","email");
+
+
+const FatArrow3 = (lastName, email) => { let a = (`Merhaba ${lastName} ${email}`);
+console.log(a);
+return a;}
+FatArrow2("LASTNAME FAT ARROW2","email2");
