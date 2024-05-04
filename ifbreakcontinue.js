f979ad2cfd929645792a0459c59d160ba0361462
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-05-04 07:30:36 +0300

    breakcontinue if

---
+++ ifbreakcontinue.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let names = ["Robert","Daniele","Florian","Tulio"]
+
+const ifuserListDOM = document.querySelector('#userList')
+
+
+let index = 0
+for(;index < names.length;index++){
+    if(names[index] == "Florian"){continue}
+    const liDOM = document.createElement('li')
+    liDOM.innerHTML = names[index]
+    ifuserListDOM.appendChild(liDOM)
+
+}
