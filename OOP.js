18bdc0488379fd2b4fc0d72fba66727a70fb1372
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-05-14 20:41:03 +0300

    oop entry level

--- OOP.js
+++ OOP.js
@@ -12,4 +12,22 @@ console.log(laptop1.model)
 console.log(laptop1["2kg"])
 
 laptop1.brand = "Mac"
+laptop1.version = "10.5.2"
 console.log(laptop1)
+
+const keys = Object.keys(laptop1) //anahtar bilgilerine ulasmak icin Object.keys() values icin Object.values()
+
+keys.forEach(keyInfo => {
+    console.log(keyInfo)
+    console.log(laptop1[keyInfo])
+})
+
+let user1 = {
+    firstName : "Deneme",
+    lastName : "Denemeoglu",
+    score : [1,2,3,4,5],
+    isActive : true,
+    longName : function() { return `${this.firstName[0].toUpperCase()} ${this.lastName}`}
+}
+
+console.log(user1.longName())
