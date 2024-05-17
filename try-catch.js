873c1260acee5e783a99aeca2a2283394f9f4b27
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-05-17 19:10:31 +0300

    try-catch

---
+++ try-catch.js
@@ -0,0 +1,13 @@
+let products
+
+try {
+    products.array.forEach(item => console.log(item));
+} catch (error) {
+    console.log("error",error) //errorun cinsinide verdi. bu alanda farklı bi islem de yaptırabiliyoruz
+}
+
+
+console.log("Hata yonetimi calisiyor")
+
+let info = "blabla"
+console.log(info)
